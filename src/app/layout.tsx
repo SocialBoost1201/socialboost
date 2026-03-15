7be6563d86872a6e84a53bc0788ef2eee3167c94
Takuma Shinnyo <takumashinnyo@MacBook-Pro.local>
2026-03-15 17:24:43 +0900

    feat: Microsoft Clarity (vw262t55jj) を組み込み

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import { SmoothScroll } from "@/components/layout/SmoothScroll";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 const notoSansJP = Noto_Sans_JP({
   variable: "--font-noto-sans-jp",
@@ -80,6 +81,17 @@ export default function RootLayout({
         </SmoothScroll>
       </body>
       <GoogleAnalytics gaId="G-Y986CYNENJ" />
+      <Script
+        id="microsoft-clarity"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "vw262t55jj");`,
+        }}
+      />
     </html>
   );
 }

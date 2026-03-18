a6c34323a289bf75c02ed39576fc3c4127c34590
Takuma Shinnyo <takumashinnyo@MacBook-Pro.local>
2026-03-18 22:05:01 +0900

    fix: Tawk.toのIDを直接コードに埋め込み（環境変数不要）

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -100,25 +100,23 @@ export default function RootLayout({
         }}
       />
       {/* Tawk.to チャットウィジェット（完全無料） */}
-      {process.env.NEXT_PUBLIC_TAWKTO_PROPERTY_ID && process.env.NEXT_PUBLIC_TAWKTO_WIDGET_ID && (
-        <Script
-          id="tawkto-chat"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/${process.env.NEXT_PUBLIC_TAWKTO_PROPERTY_ID}/${process.env.NEXT_PUBLIC_TAWKTO_WIDGET_ID}';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-              })();
-            `,
-          }}
-        />
-      )}
+      <Script
+        id="tawkto-chat"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/69baa12d65362b1c3680ab22/1jk0gb6iq';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `,
+        }}
+      />
     </html>
   );
 }
